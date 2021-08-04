523aaf50584013de450ff4ac879340ffe33c6aca
jiayu <350600880@qq.com>
2021-08-04 14:39:15 +0800

    [jiayu] fix edit requirement perfection and label input conflict and install jest

---
+++ todoMVC/src/app.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {screen,render} from "@testing-library/react";
+import'@testing-library/jest-dom'
+import App from "./app";
+
+describe('App',()=>{
+    test('should render todo list',()=>{
+        render(<App/>);
+        expect(screen.getByText(/TodoMVC/i)).toBeInTheDocument();
+    })
+})
--- todoMVC/src/components/TodoList/TodoItems.js
+++ todoMVC/src/components/TodoList/TodoItems.js
@@ -1,13 +1,17 @@
 import React, {useState} from "react";
 import './index.css'
 
-const TodoItems = ({todo, todos, handleComplete, deleteTodo,editTodoList}) => {
+const TodoItems = ({todo,handleComplete, deleteTodo,editTodoList}) => {
     const[isEdit,setIsEdit]=useState(false)
-    const[name,setName]=useState('')
+    const[name,setName]=useState(todo.name)
 
-    const handleKeyDown=(e,todo)=>{
-        if(e.keyCode===13){
+    const handleKeyUp=(e,todo,name)=>{
+        const reg = new RegExp(/^\s+$/)
+        if(e.keyCode===13 && !reg.test(name) && name.length > 0){
             editTodoList(todo.id,name)
+            setIsEdit(false)
+        }else if(name.length===0 && e.keyCode===13  ){
+            deleteTodo(todo.id)
             setIsEdit(false)
         }
     }
@@ -17,24 +21,24 @@ const TodoItems = ({todo, todos, handleComplete, deleteTodo,editTodoList}) => {
                 < li key={todo.id} className={(todo.isComplete) ? 'completed' : 'none'} >
                     { !isEdit &&< div className='view'>
                         <input className='toggle' type='checkbox'
-                               id='checkbox'
-                               onChange={(e) => handleComplete(todo, e)}
+                               onChange={(e) => {
+                                   handleComplete(todo, e)
+                               }}
                         />
                         <label
-                            htmlFor='checkbox'
                             className={todo.isComplete?'checkboxChecked':'checkboxUnchecked'}
                             onDoubleClick={()=>setIsEdit(true)}
                           >{name===''?todo.name:name}</label>
                         <button className='destroy' onClick={() => deleteTodo(todo.id)}>x</button>
                     </div>}
-                    { isEdit && <input className='edit' value={name===''?todo.name:name}
-                                      onKeyDown={(e)=>handleKeyDown(e,todo)}
+                    { isEdit && <input className='edit' value={name}
+                                      onKeyUp={(e)=>handleKeyUp(e,todo,name)}
                                       onChange={(e) => setName(e.target.value)}
                     />}
                 </li>
             }
         </>
     )
-};
+}
 
 export default TodoItems
